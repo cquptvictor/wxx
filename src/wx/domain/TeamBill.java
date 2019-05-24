5e192edf9d510102527aa3fe7083679b966e1cc5
victor <943382615@qq.com>
2019-05-24 15:44:51 +0800

    添加了获取账单的功能

---
+++ src/wx/domain/TeamBill.java
@@ -0,0 +1,58 @@
+package wx.domain;
+
+public class TeamBill {
+    private String bid;
+    private String uid;
+    private String nickName;
+    private String amount;
+    private String  label;
+    private String remarks;
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
---
+++ src/wx/team/getTeamBill.java
@@ -0,0 +1,32 @@
+package wx.team;
+
+import com.google.gson.Gson;
+import wx.domain.TeamBill;
+import wx.service.TeamService;
+import wx.utils.JsonUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "Servlet16")
+public class getTeamBill extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String tid = request.getParameter("tid");
+        String  page = request.getParameter("page");
+        PrintWriter printWriter = response.getWriter();
+        Gson gson = JsonUtils.getGson();
+        TeamService teamService = new TeamService();
+        List<TeamBill> list = teamService.getTeamBill(tid,page);
+        printWriter.write("{'data':["+gson.toJson(list)+"]}");
+    }
+}
