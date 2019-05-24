88bcf957ef50c542778fd3c9e10e5126e39821ea
victor <943382615@qq.com>
2019-05-24 21:33:45 +0800

    添加了删除成员的功能，以及一个验证发起删除者是否是管理员

---
+++ src/wx/controller/team/delTeamBill.java
@@ -0,0 +1,31 @@
+package wx.controller.team;
+
+import wx.service.TeamService;
+import wx.utils.isAdministrator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet19")
+public class delTeamBill extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String openId = (String)request.getSession().getAttribute("token");
+        String bid = request.getParameter("bid");
+        String tid = request.getParameter("tid");
+        PrintWriter printWriter = response.getWriter();
+        TeamService teamService = new TeamService();
+        if(isAdministrator.auth(openId,tid) && teamService.delTeamBill(tid,bid)){
+            printWriter.write("{'static':1}");
+        }else
+            printWriter.write("{'static':0}");
+    }
+}
---
+++ src/wx/utils/isAdministrator.java
@@ -0,0 +1,22 @@
+package wx.utils;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayHandler;
+
+import java.sql.SQLException;
+/*
+验证该用户是否为管理员*/
+public class isAdministrator {
+    public static Boolean auth(String openId,String tid){
+        QueryRunner queryRunner = new QueryRunner(JdbcUtils.getDataSource());
+        String sql = "select isAdministrator from team where tid = ? and uid = ";
+        try {
+            if(queryRunner.query(sql,new ArrayHandler(),new Object[]{openId,tid})[0] != null)
+                return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+}
