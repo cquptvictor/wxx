1484c237dfe387a8d64125b304a487533a69e4fa
victor <943382615@qq.com>
2019-05-26 22:38:19 +0800

    解散团队，在通知和账单没有记录时是无法成功的

---
+++ src/wx/controller/team/dismissTeam.java
@@ -0,0 +1,36 @@
+package wx.controller.team;
+
+import wx.service.TeamService;
+import wx.utils.auth.isAdministrator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet11")
+public class dismissTeam extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String tid = request.getParameter("tid");
+        String openId = (String)request.getSession().getAttribute("token");
+        PrintWriter printWriter = response.getWriter();
+        //只有管理员才可以解散团队
+        if(isAdministrator.auth(openId,tid)){
+            TeamService teamService = new TeamService();
+            if(teamService.dismissTeam(tid))
+                printWriter.write("{'static':1}");
+            else
+                printWriter.write("{'static':0}");
+        }else
+            printWriter.write("{'static':0}");
+
+    }
+}
